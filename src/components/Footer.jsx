069c57baeba6b83b7f1d3ab6b389bea9f3a1c2d0
Afsana Zaman Nipa <afsana2013nipa@gmail.com>
2022-03-20 16:39:09 +0600

    Footer has been added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>স্বত্ব &copy; ২০২২ প্রথম আলো
+সম্পাদক ও প্রকাশক: মতিউর রহমান</div>
+  )
+}
+
+export default Footer
--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import MainNewsContainer from './MainNewsContainer'
+import Footer from './Footer'
 
 function FrontPage() {
   return (
     <div>
       <Header />
       <MainNewsContainer />
+      <Footer />
     </div>
   )
 }
